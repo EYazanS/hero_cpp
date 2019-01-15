7687a09746e0a81e6a8f0e7b9a59ef282927d7d0
Yazan Salameh <e.yazan@live.com>
2019-01-16 00:47:23 +0200

    Preparing window for rendering

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 
 LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam);
 
+// TODO: change from global variable
+#pragma region globals_variables
+static bool Runnig = true;
+static BITMAPINFO BitMapInfo;
+static void *BitMapMemory;
+static  HBITMAP BitMapHandle;
+static HDC BitMapDeviceContext;
+#pragma endregion
+
+void Win32ResizeDIBSection(int height, int width);
+void Win32UpdateWindow(HDC DevicContext, int x, int y, int height, int width);
+
 // Entry point
 int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR CommandLine, int ShowCode)
 {
@@ -38,10 +50,9 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR Comma
 			MSG message;
 
 			// Change for future
-			for (; ;)
+			while (Runnig)
 			{
 				BOOL messageResult = GetMessage(&message, 0, 0, 0);
-
 				if (messageResult > 0)
 				{
 					TranslateMessage(&message);
@@ -49,7 +60,8 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR Comma
 				}
 				else
 				{
-					break;
+					// Exit loop
+					Runnig = false;
 				}
 			}
 		}
@@ -65,29 +77,36 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR Comma
 		// TODO: Logging
 	}
 
-	auto box = MessageBox(0, "Heroes are self made", "Hero", MB_OKCANCEL | MB_ICONINFORMATION);
+	// int box = MessageBox(0, "Heroes are self made", "Hero", MB_OKCANCEL | MB_ICONINFORMATION);
 
 	return 0;
 }
 
-LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam) {
+LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
 	LRESULT result = 0;
 
 	switch (Message)
 	{
 		case WM_SIZE:
 		{
+			RECT clientRect;
+			GetClientRect(Window, &clientRect);
+			int height = clientRect.bottom - clientRect.top;
+			int width = clientRect.right - clientRect.left;
+			Win32ResizeDIBSection(height, width);
 			OutputDebugString("Resize\n");
-		} break;
-
-		case WM_DESTROY:
-		{
-			OutputDebugString("Desatroyed\n");
 		} break;
 
 		case WM_CLOSE:
 		{
-			OutputDebugString("Closes\n");
+			Runnig = false;
+		} break;
+
+		case WM_DESTROY:
+		{
+			// TODO: Hand as error - try to create window
+			Runnig = false;
 		} break;
 
 		case WM_ACTIVATEAPP:
@@ -103,6 +122,8 @@ LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LP
 			int y = paint.rcPaint.top;
 			int height = paint.rcPaint.bottom - paint.rcPaint.top;
 			int width = paint.rcPaint.right - paint.rcPaint.left;
+			Win32UpdateWindow(DevicContext, x, y, height, width);
+
 			PatBlt(DevicContext, x, y, width, height, WHITENESS);
 			EndPaint(Window, &paint);
 		}break;
@@ -111,10 +132,37 @@ LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LP
 		{
 			// return window default behaviour
 			result = DefWindowProc(Window, Message, WParam, LParam);
-
 		} break;
 
 	}
 
 	return (result);
+}
+
+void Win32ResizeDIBSection(int height, int width)
+{
+	if (BitMapHandle)
+		DeleteObject(BitMapHandle);
+	
+	if(!BitMapDeviceContext)
+		BitMapDeviceContext = CreateCompatibleDC(0);
+
+	BitMapInfo.bmiHeader.biSize = sizeof(BitMapInfo.bmiHeader);
+	BitMapInfo.bmiHeader.biWidth = width;
+	BitMapInfo.bmiHeader.biHeight = height;
+	BitMapInfo.bmiHeader.biPlanes = 1;
+	BitMapInfo.bmiHeader.biBitCount = 32;
+	BitMapInfo.bmiHeader.biCompression = BI_RGB;
+	BitMapInfo.bmiHeader.biSizeImage = 0;
+	BitMapInfo.bmiHeader.biXPelsPerMeter = 0;
+	BitMapInfo.bmiHeader.biYPelsPerMeter = 0;
+	BitMapInfo.bmiHeader.biClrUsed = 0;
+	BitMapInfo.bmiHeader.biClrImportant = 0;
+
+	HBITMAP section = CreateDIBSection(BitMapDeviceContext, &BitMapInfo, DIB_RGB_COLORS, &BitMapMemory, 0, 0);
+}
+
+void Win32UpdateWindow(HDC DevicContext, int x, int y, int height, int width)
+{
+	int result = StretchDIBits(DevicContext, x, y, width, height, x, y, width, height, BitMapMemory, &BitMapInfo, DIB_RGB_COLORS, SRCCOPY);
 }
