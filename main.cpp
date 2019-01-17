b86ad7ded4627d3ac1cc1b2a474f8a35e26341ba
Yazan Salameh <e.yazan@live.com>
2019-01-17 20:10:57 +0200

    Cleaned code and main streamed how to get window dimensions

--- main.cpp
+++ main.cpp
@@ -11,29 +11,44 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
-// TODO: change from global variable
-#pragma region globals_variables
+struct win32_offscreen_buffer
+{
+	BITMAPINFO Info;
+	void *Memory;
+	int Height;
+	int Width;
+	int BytesPerPixel;
+	int Pitch;
+};
+
+struct win32_window_dimensions
+{
+	int Width;
+	int Height;
+};
+
 static bool Runnig = true;
-static BITMAPINFO BitMapInfo;
-static void *BitMapMemory;
-static int BitMapHeight = 0;
-static int BitMapWidth = 0;
-const int BytesPerPixed = 4;
+static win32_offscreen_buffer GlobalBackBuffer;
 
-#pragma endregion
-
-void Win32ResizeDIBSection(int Height, int Width);
-void Win32UpdateWindow(HDC DevicContext, RECT* WindowRect);
-void RenderGradiant(int XOffset, int YOffset);
+void Win32ResizeDIBSection(win32_offscreen_buffer* Buffer, int Height, int Width);
+void Win32DisplayBufferInWindow(win32_offscreen_buffer Buffer, HDC DevicContext, win32_window_dimensions WindowDimensions);
+void RenderGradiant(win32_offscreen_buffer* Buffer, int XOffset, int YOffset);
+win32_window_dimensions Win32GetWindowDimensions(HWND Window);
 LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam);
 
 // Entry point
 int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR CommandLine, int ShowCode)
 {
+	// Initialize Window to 0
 	WNDCLASS windowsClass = {};
 
-	//CS_HREDRAW|CS_VREDRAW redraw the window when it gets dragged h => horizontal | v -> vertical
-	windowsClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	// Stack overflow exceptinos
+	// uint8 memory[2 * 1024 * 1024] = {};
+
+	Win32ResizeDIBSection(&GlobalBackBuffer, 1280, 720);
+
+	// CS_HREDRAW|CS_VREDRAW redraw the entire window when it gets dragged, h => horizontal | v -> vertical
+	windowsClass.style = CS_HREDRAW | CS_VREDRAW;
 	windowsClass.hInstance = Instance;
 	windowsClass.lpszClassName = "Class Hero game";
 	windowsClass.lpfnWndProc = MainWindowCallBack;
@@ -72,11 +87,10 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR Comma
 					DispatchMessage(&message);
 				}
 
-				RenderGradiant(xOffset++, 0);
+				RenderGradiant(&GlobalBackBuffer, xOffset++, 0);
 				HDC deviceContext = GetDC(window);
-				RECT clientRect;
-				GetClientRect(window, &clientRect);
-				Win32UpdateWindow(deviceContext, &clientRect);
+				win32_window_dimensions dimensions = Win32GetWindowDimensions(window);
+				Win32DisplayBufferInWindow(GlobalBackBuffer, deviceContext, dimensions);
 				ReleaseDC(window, deviceContext);
 			}
 		}
@@ -104,12 +118,7 @@ LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LP
 	{
 		case WM_SIZE:
 		{
-			RECT clientRect;
-			GetClientRect(Window, &clientRect);
-			int height = clientRect.bottom - clientRect.top;
-			int width = clientRect.right - clientRect.left;
-			Win32ResizeDIBSection(height, width);
-			OutputDebugString("Resize\n");
+
 		} break;
 
 		case WM_CLOSE:
@@ -119,27 +128,20 @@ LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LP
 
 		case WM_DESTROY:
 		{
-			// TODO: Hand as error - try to create window
+			// TODO: Handle it as an error - try to re-create window
 			Runnig = false;
 		} break;
 
 		case WM_ACTIVATEAPP:
 		{
-			OutputDebugString("activated\n");
 		} break;
 
 		case WM_PAINT:
 		{
 			PAINTSTRUCT paint;
 			HDC devicContext = BeginPaint(Window, &paint);
-			int x = paint.rcPaint.left;
-			int y = paint.rcPaint.top;
-			int height = paint.rcPaint.bottom - paint.rcPaint.top;
-			int width = paint.rcPaint.right - paint.rcPaint.left;
-			RECT clientRect;
-			GetClientRect(Window, &clientRect);
-			Win32UpdateWindow(devicContext, &clientRect);
-			//PatBlt(devicContext, x, y, width, height, WHITENESS);
+			win32_window_dimensions dimensions = Win32GetWindowDimensions(Window);
+			Win32DisplayBufferInWindow(GlobalBackBuffer, devicContext, dimensions);
 			EndPaint(Window, &paint);
 		}break;
 
@@ -154,54 +156,66 @@ LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LP
 	return (result);
 }
 
-void Win32ResizeDIBSection(int Height, int Width)
+void Win32ResizeDIBSection(win32_offscreen_buffer* Buffer, int Height, int Width)
 {
-	if (BitMapMemory)
-		VirtualFree(BitMapMemory, 0, MEM_RELEASE);
+	if (Buffer->Memory)
+		VirtualFree(Buffer->Memory, 0, MEM_RELEASE);
 
-	BitMapWidth = Width;
-	BitMapHeight = Height;
+	Buffer->Width = Width;
+	Buffer->Height = Height;
+	Buffer->BytesPerPixel = 4;
 
-	BitMapInfo.bmiHeader.biSize = sizeof(BitMapInfo.bmiHeader);
-	BitMapInfo.bmiHeader.biWidth = BitMapWidth;
-	BitMapInfo.bmiHeader.biHeight = -BitMapHeight;
-	BitMapInfo.bmiHeader.biPlanes = 1;
-	BitMapInfo.bmiHeader.biBitCount = 32;
-	BitMapInfo.bmiHeader.biCompression = BI_RGB;
+	Buffer->Info.bmiHeader.biSize = sizeof(Buffer->Info.bmiHeader);
+	Buffer->Info.bmiHeader.biWidth = Buffer->Width;
+	Buffer->Info.bmiHeader.biHeight = -Buffer->Height;
+	Buffer->Info.bmiHeader.biPlanes = 1;
+	Buffer->Info.bmiHeader.biBitCount = 32;
+	Buffer->Info.bmiHeader.biCompression = BI_RGB;
 
 	// 8 bits as padding because we have 8 bits green, 8 bits red and 8 bits blue equal 3 bytes
 	// so we need 8 more bits to keep performance the 8 bits wont be used at all
-	int bitMapMemory = (BitMapWidth * BitMapHeight) * BytesPerPixed;
-	BitMapMemory = VirtualAlloc(0, bitMapMemory, MEM_COMMIT, PAGE_READWRITE);
+	int bitMapMemory = (Buffer->Width * Buffer->Height) * Buffer->BytesPerPixel;
+	Buffer->Memory = VirtualAlloc(0, bitMapMemory, MEM_COMMIT, PAGE_READWRITE);
+	Buffer->Pitch = Buffer->Width * Buffer->BytesPerPixel;
 }
 
-void Win32UpdateWindow(HDC DevicContext, RECT* WindowRect)
+void Win32DisplayBufferInWindow(win32_offscreen_buffer Buffer, HDC DevicContext, win32_window_dimensions WindowDimensions)
 {
-	int windowWidth = WindowRect->right - WindowRect->left;
-	int windowHeight = WindowRect->bottom - WindowRect->top;
-	int result = StretchDIBits(DevicContext, 0, 0, BitMapWidth, BitMapHeight, 0, 0, windowWidth, windowHeight, BitMapMemory, &BitMapInfo, DIB_RGB_COLORS, SRCCOPY);
+	// TODO: Fix aspect ratio
+	StretchDIBits(DevicContext, 
+		0, 0, WindowDimensions.Width, WindowDimensions.Height, // Distenation
+		0, 0, Buffer.Width, Buffer.Height, // Source
+		Buffer.Memory, &Buffer.Info, DIB_RGB_COLORS, SRCCOPY);
 }
 
-void RenderGradiant(int XOffset, int YOffset)
+void RenderGradiant(win32_offscreen_buffer* Buffer, int XOffset, int YOffset)
 {
-	int pitch = BitMapWidth * BytesPerPixed;
-	uint8* row = (uint8*)BitMapMemory;
+	uint8* row = (uint8*)Buffer->Memory;
 
-	for (int y = 0; y < BitMapHeight; ++y)
+	for (int y = 0; y < Buffer->Height; ++y)
 	{
 		uint32* pixel = (uint32*)row;
 
-		for (int x = 0; x < BitMapWidth; ++x)
+		for (int x = 0; x < Buffer->Width; ++x)
 		{
 			uint8 blue = (uint8)x + XOffset;
 			uint8 green = (uint8)y + YOffset;
 			uint8 red = 100;
-			uint8 padding = 0;
+			uint8 alpha = 255;
 
 			// Blue
-			*pixel++ = (red << 16)| (green << 8) | blue;
+			*pixel++ = (alpha << 24) | (red << 16) | (green << 8) | blue;
 		}
 
-		row += pitch;
+		row += Buffer->Pitch;
 	}
+}
+
+win32_window_dimensions Win32GetWindowDimensions(HWND Window) {
+	win32_window_dimensions dimensions;
+	RECT clientRect;
+	GetClientRect(Window, &clientRect);
+	dimensions.Height = clientRect.bottom - clientRect.top;
+	dimensions.Width = clientRect.right - clientRect.left;
+	return dimensions;
 }
