deba3ddad65ae8d1a5511e2bf9af5fdf9ea01a9d
Yazan Salameh <e.yazan@live.com>
2019-01-17 13:39:07 +0200

    Added test gradiants and animations

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include<Windows.h>
+#include<stdint.h>
 
-LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam);
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
 // TODO: change from global variable
 #pragma region globals_variables
 static bool Runnig = true;
 static BITMAPINFO BitMapInfo;
 static void *BitMapMemory;
-static  HBITMAP BitMapHandle;
-static HDC BitMapDeviceContext;
+static int BitMapHeight = 0;
+static int BitMapWidth = 0;
+const int BytesPerPixed = 4;
+
 #pragma endregion
 
-void Win32ResizeDIBSection(int height, int width);
-void Win32UpdateWindow(HDC DevicContext, int x, int y, int height, int width);
+void Win32ResizeDIBSection(int Height, int Width);
+void Win32UpdateWindow(HDC DevicContext, RECT* WindowRect);
+void RenderGradiant(int XOffset, int YOffset);
+LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam);
 
 // Entry point
 int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR CommandLine, int ShowCode)
@@ -22,7 +35,7 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR Comma
 	//CS_HREDRAW|CS_VREDRAW redraw the window when it gets dragged h => horizontal | v -> vertical
 	windowsClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
 	windowsClass.hInstance = Instance;
-	windowsClass.lpszClassName = "Hero game";
+	windowsClass.lpszClassName = "Class Hero game";
 	windowsClass.lpfnWndProc = MainWindowCallBack;
 
 	//WindowsClass.lpfnWndProc = MainWindowCallBack;
@@ -31,7 +44,7 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR Comma
 
 	if (RegisterClass(&windowsClass))
 	{
-		HWND windowHandle = CreateWindowEx(
+		HWND window = CreateWindowEx(
 			0,
 			windowsClass.lpszClassName,
 			"Hero window",
@@ -40,29 +53,31 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR Comma
 			CW_USEDEFAULT,
 			CW_USEDEFAULT,
 			CW_USEDEFAULT,
-			0,
-			0,
-			Instance, 0);
+			0, 0, Instance, 0);
 
-		if (windowHandle)
+		if (window)
 		{
 			// Extract messages from windows
 			MSG message;
-
+			int xOffset = 0;
 			// Change for future
 			while (Runnig)
 			{
-				BOOL messageResult = GetMessage(&message, 0, 0, 0);
-				if (messageResult > 0)
+				while (PeekMessage(&message, 0, 0, 0, PM_REMOVE))
 				{
+					if (message.message == WM_QUIT)
+						Runnig = false;
+
 					TranslateMessage(&message);
 					DispatchMessage(&message);
 				}
-				else
-				{
-					// Exit loop
-					Runnig = false;
-				}
+
+				RenderGradiant(xOffset++, 0);
+				HDC deviceContext = GetDC(window);
+				RECT clientRect;
+				GetClientRect(window, &clientRect);
+				Win32UpdateWindow(deviceContext, &clientRect);
+				ReleaseDC(window, deviceContext);
 			}
 		}
 		else
@@ -78,7 +93,6 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR Comma
 	}
 
 	// int box = MessageBox(0, "Heroes are self made", "Hero", MB_OKCANCEL | MB_ICONINFORMATION);
-
 	return 0;
 }
 
@@ -117,14 +131,15 @@ LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LP
 		case WM_PAINT:
 		{
 			PAINTSTRUCT paint;
-			HDC DevicContext = BeginPaint(Window, &paint);
+			HDC devicContext = BeginPaint(Window, &paint);
 			int x = paint.rcPaint.left;
 			int y = paint.rcPaint.top;
 			int height = paint.rcPaint.bottom - paint.rcPaint.top;
 			int width = paint.rcPaint.right - paint.rcPaint.left;
-			Win32UpdateWindow(DevicContext, x, y, height, width);
-
-			PatBlt(DevicContext, x, y, width, height, WHITENESS);
+			RECT clientRect;
+			GetClientRect(Window, &clientRect);
+			Win32UpdateWindow(devicContext, &clientRect);
+			//PatBlt(devicContext, x, y, width, height, WHITENESS);
 			EndPaint(Window, &paint);
 		}break;
 
@@ -139,30 +154,54 @@ LRESULT CALLBACK MainWindowCallBack(HWND Window, UINT Message, WPARAM WParam, LP
 	return (result);
 }
 
-void Win32ResizeDIBSection(int height, int width)
+void Win32ResizeDIBSection(int Height, int Width)
 {
-	if (BitMapHandle)
-		DeleteObject(BitMapHandle);
-	
-	if(!BitMapDeviceContext)
-		BitMapDeviceContext = CreateCompatibleDC(0);
+	if (BitMapMemory)
+		VirtualFree(BitMapMemory, 0, MEM_RELEASE);
+
+	BitMapWidth = Width;
+	BitMapHeight = Height;
 
 	BitMapInfo.bmiHeader.biSize = sizeof(BitMapInfo.bmiHeader);
-	BitMapInfo.bmiHeader.biWidth = width;
-	BitMapInfo.bmiHeader.biHeight = height;
+	BitMapInfo.bmiHeader.biWidth = BitMapWidth;
+	BitMapInfo.bmiHeader.biHeight = -BitMapHeight;
 	BitMapInfo.bmiHeader.biPlanes = 1;
 	BitMapInfo.bmiHeader.biBitCount = 32;
 	BitMapInfo.bmiHeader.biCompression = BI_RGB;
-	BitMapInfo.bmiHeader.biSizeImage = 0;
-	BitMapInfo.bmiHeader.biXPelsPerMeter = 0;
-	BitMapInfo.bmiHeader.biYPelsPerMeter = 0;
-	BitMapInfo.bmiHeader.biClrUsed = 0;
-	BitMapInfo.bmiHeader.biClrImportant = 0;
 
-	HBITMAP section = CreateDIBSection(BitMapDeviceContext, &BitMapInfo, DIB_RGB_COLORS, &BitMapMemory, 0, 0);
+	// 8 bits as padding because we have 8 bits green, 8 bits red and 8 bits blue equal 3 bytes
+	// so we need 8 more bits to keep performance the 8 bits wont be used at all
+	int bitMapMemory = (BitMapWidth * BitMapHeight) * BytesPerPixed;
+	BitMapMemory = VirtualAlloc(0, bitMapMemory, MEM_COMMIT, PAGE_READWRITE);
 }
 
-void Win32UpdateWindow(HDC DevicContext, int x, int y, int height, int width)
+void Win32UpdateWindow(HDC DevicContext, RECT* WindowRect)
 {
-	int result = StretchDIBits(DevicContext, x, y, width, height, x, y, width, height, BitMapMemory, &BitMapInfo, DIB_RGB_COLORS, SRCCOPY);
+	int windowWidth = WindowRect->right - WindowRect->left;
+	int windowHeight = WindowRect->bottom - WindowRect->top;
+	int result = StretchDIBits(DevicContext, 0, 0, BitMapWidth, BitMapHeight, 0, 0, windowWidth, windowHeight, BitMapMemory, &BitMapInfo, DIB_RGB_COLORS, SRCCOPY);
+}
+
+void RenderGradiant(int XOffset, int YOffset)
+{
+	int pitch = BitMapWidth * BytesPerPixed;
+	uint8* row = (uint8*)BitMapMemory;
+
+	for (int y = 0; y < BitMapHeight; ++y)
+	{
+		uint32* pixel = (uint32*)row;
+
+		for (int x = 0; x < BitMapWidth; ++x)
+		{
+			uint8 blue = (uint8)x + XOffset;
+			uint8 green = (uint8)y + YOffset;
+			uint8 red = 100;
+			uint8 padding = 0;
+
+			// Blue
+			*pixel++ = (red << 16)| (green << 8) | blue;
+		}
+
+		row += pitch;
+	}
 }
